108df46a46e1774b6d76f1e80d427681a26e419e
Nikita-Vaid <vaidnikita06@gmail.com>
2024-06-30 02:45:34 +0530

    final changes

--- build/static/js/482.b9c4fab7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkchat=self.webpackChunkchat||[]).push([[482],{7482:(c,e,s)=>{s.r(e),s.d(e,{default:()=>t});const t=s(3502).z}}]);
-//# sourceMappingURL=482.b9c4fab7.chunk.js.map
--- src/components/hook-form/index.js
+++
@@ -1,5 +0,0 @@
-export * from './'
-export { default } from './FormProvider';
-
-
-export { default as RHFTextField } from './RHFTextField';
